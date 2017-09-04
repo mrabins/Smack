6da39314cf8cdb5fcd2bb26b245e95750d1f661b
Mark Rabins <markrabins@gmail.com>
2017-09-03 20:55:17 -0700

    Storyboards and their respective classes for Chat and Channel, Completed inital menu set up

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,19 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Mark Rabins on 9/3/17.
+//  Copyright © 2017 self. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+
+
+    }
+}
---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,30 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Mark Rabins on 9/3/17.
+//  Copyright © 2017 self. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    //Outlets
+    
+    @IBOutlet weak var menuButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuButton.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+        
+    }
+
+
+ 
+}
