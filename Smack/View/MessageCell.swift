a22fa2dbe915a1c7befeddde118cebd2925e24eb
Mark Rabins <markrabins@gmail.com>
2017-12-26 21:53:05 -0800

    Updated timestamp

--- Smack/View/MessageCell.swift
+++ Smack/View/MessageCell.swift
@@ -27,7 +27,20 @@ class MessageCell: UITableViewCell {
         userImage.image = UIImage(named: message.userAvatar)
         userImage.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
         timeStampLabel.text = ""
+        
+        guard var isoDate = message.timeStamp else { return }
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = String(isoDate[end])
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate)
+            timeStampLabel.text = finalDate
+        }
     }
-    
-
 }
